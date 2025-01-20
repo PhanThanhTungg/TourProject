2b8a0207da432b459b33e4913bb9a1bea0f73812
TungPhan03 <tuttutxytin@gmail.com>
2025-01-20 18:36:02 +0700

    category page

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import tourRoute from "./tour.route";
+import categoryRoute from "./category.route";
 
 export default (app:Express)=>{
   app.use("/tours", tourRoute);
+  app.use("/categories", categoryRoute);
 }
