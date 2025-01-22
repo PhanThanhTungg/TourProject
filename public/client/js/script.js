87bb65cf13d05c0f5acee85e3859e2f59ff5e53a
TungPhan03 <tuttutxytin@gmail.com>
2025-01-22 09:49:07 +0700

    add to cart

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -1,1 +1,22 @@
-console.log("ok");
+const cart = localStorage.getItem("cart");
+if(!cart) localStorage.setItem("cart", JSON.stringify([]));
+
+
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart){
+  formAddToCart.addEventListener("submit", e=>{
+    e.preventDefault();
+    const tourId = formAddToCart.getAttribute("tour-id");
+    const quantity = e.target.quantity.value;
+    if(tourId && quantity){
+      const cart = JSON.parse(localStorage.getItem("cart"));
+      const checkIndex = cart.findIndex(item=> item.tourId == tourId);
+      if(checkIndex!=-1) cart[checkIndex].quantity += parseInt(quantity);
+      else cart.push({
+        tourId: tourId,
+        quantity: parseInt(quantity)
+      });
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  })
+}
