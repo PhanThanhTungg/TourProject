3bfea87154df597017f63212221146d67c617497
TungPhan03 <tuttutxytin@gmail.com>
2025-02-01 17:36:51 +0700

    create successfull order interface

--- routes/client/order.route.ts
+++ routes/client/order.route.ts
@@ -4,5 +4,6 @@ const router = express.Router();
 import * as controller from "../../controllers/client/order.controller";
 
 router.post("/", controller.index);
+router.get("/success", controller.success);
 
 export default router;
