81df8207240aa09e3997b802ae53c7994c8455d3
TungPhan03 <tuttutxytin@gmail.com>
2025-02-01 16:49:15 +0700

    save data to table-order

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,9 +1,34 @@
 import { Request, Response } from "express";
+import Order from "../../model/order.model";
 
 export const index = async (req: Request, res: Response) => {
-  console.log(req.body);
+  const info = req.body.info;
+  const cart = req.body.cart;
+
+  const dataOrder = {
+    code: "",
+    fullName: info.fullName,
+    phone: info.phone,
+    note: info.note,
+    status: "initial",
+  };
+
+  const order = await Order.create(dataOrder);
+  const orderId = order.dataValues.id;
+
+  const code = `OD${String(orderId).padStart(8,'0')}`
+
+  await Order.update({
+    code: code
+  }, {
+    where: {
+      id: orderId
+    }
+  });
+
 
   res.json({
-    mess: "ok"
+    code: 200,
+    orderCode: code
   });
 };
