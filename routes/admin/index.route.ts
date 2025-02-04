57966848b06c1ba9b24f8714f8f6e61ec7ad69c5
TungPhan03 <tuttutxytin@gmail.com>
2025-02-04 22:39:45 +0700

    create tour page for admin

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 import { systemConfig } from "../../config/system";
 import categoryRoute from "./category.route";
+import tourRoute from "./tour.route";
 
 export default (app: Express)=>{
   const path = `/${systemConfig.prefixAdmin}`
 
   app.use(`${path}/categories`, categoryRoute);
+  app.use(`${path}/tours`, tourRoute);
 }
